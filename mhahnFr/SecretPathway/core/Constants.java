5d8a462ff31e94045420930a5684a87b40649051
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-11-02 19:05:11 +0100

    Added class for constants used throughout the project

---
+++ mhahnFr/SecretPathway/core/Constants.java
@@ -0,0 +1,31 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2022  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.core;
+
+/**
+ * This class contains some constant values used in the whole project.
+ *
+ * @since 02.11.2022
+ * @author mhahnFr
+ */
+public final class Constants {
+    /** The name to be used inside the project. */
+    public static final String NAME = "SecretPathway";
+}
