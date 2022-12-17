da39b3697f63f52feaa2412d29913bee53d5af95
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-12-17 22:45:48 +0100

    Documented the ProtocolPlugin

--- src/mhahnFr/SecretPathway/core/protocols/ProtocolPlugin.java
+++ src/mhahnFr/SecretPathway/core/protocols/ProtocolPlugin.java
@@ -22,13 +22,35 @@ package mhahnFr.SecretPathway.core.protocols;
 import mhahnFr.SecretPathway.core.net.ConnectionSender;
 
 /**
- * TODO: Write documentation
+ * This interface defines the functionality a protocol plugin has
+ * to fulfil.
  *
  * @author mhahnFr
  * @since 17.12.22
  */
 public interface ProtocolPlugin {
+    /**
+     * Returns whether the given byte is to be interpreted as
+     * the beginning of a sequence this plugin handles.
+     * It is called when the state machine tries to determine
+     * the type of the received byte. If this function returns
+     * {@code true}, incoming data is sent to this plugin's
+     * {@link #process(byte, ConnectionSender)} function.
+     *
+     * @param b the byte that might be the beginning byte
+     * @return whether this plugin should be called for processing the next byte
+     */
     boolean isBegin(byte b);
 
+    /**
+     * Called when a new byte is received and this plugin has
+     * indicated that it is responsible for handling the next
+     * incoming data. It will return whether the next incoming
+     * byte belongs to this plugin.
+     *
+     * @param b the byte that was received
+     * @param sender a reference to the sender responsible for sending back a potential response
+     * @return whether this plugin should be called for the next incoming byte
+     */
     boolean process(byte b, ConnectionSender sender);
 }
