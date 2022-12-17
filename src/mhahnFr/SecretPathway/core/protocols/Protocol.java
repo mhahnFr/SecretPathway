4baa5eaf9e092a2d4dd408e2c8e6c4ba5452499c
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-12-17 22:14:36 +0100

    Added protocol abstraction package

---
+++ src/mhahnFr/SecretPathway/core/protocols/Protocol.java
@@ -0,0 +1,34 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2022  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.core.protocols;
+
+/**
+ * Instances of this class act as a protocol abstraction.
+ * This class consists of a state machine to determine the
+ * {@link ProtocolPlugin} to be used.
+ * <br>
+ * Although plugins can be added at any time, they cannot be
+ * removed.
+ *
+ * @author mhahnFr
+ * @since 17.12.22
+ */
+public class Protocol {
+}
