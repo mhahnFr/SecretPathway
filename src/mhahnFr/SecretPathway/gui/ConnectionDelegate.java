37a76765fa420fdc6579e9a81b5b936fbb7afe71
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-03 21:39:41 +0100

    Removed the old ANSI code

--- src/mhahnFr/SecretPathway/gui/ConnectionDelegate.java
+++ src/mhahnFr/SecretPathway/gui/ConnectionDelegate.java
@@ -23,6 +23,7 @@ import mhahnFr.SecretPathway.core.net.Connection;
 import mhahnFr.SecretPathway.core.net.ConnectionListener;
 import mhahnFr.SecretPathway.core.net.ConnectionSender;
 import mhahnFr.SecretPathway.core.protocols.Protocol;
+import mhahnFr.SecretPathway.core.protocols.ansi.ANSIPlugin;
 import mhahnFr.SecretPathway.core.protocols.spp.SPPPlugin;
 import mhahnFr.SecretPathway.core.protocols.telnet.TelnetPlugin;
 import mhahnFr.SecretPathway.gui.helper.MessageReceiver;
@@ -46,7 +47,7 @@ import java.util.concurrent.Future;
  * @since 21.11.2022
  * @author mhahnFr
  */
-class ConnectionDelegate implements ConnectionListener, ConnectionSender {
+public class ConnectionDelegate implements ConnectionListener, ConnectionSender {
     /** The underlying connection to be controlled.                                */
     private final Connection connection;
     /** The default style used by the main text pane.                              */
@@ -65,14 +66,11 @@ class ConnectionDelegate implements ConnectionListener, ConnectionSender {
     private Timer reconnectTimer;
     /** Indicates whether something has been received on this connection.          */
     private boolean firstReceive = true;
-    /** Indicates whether incoming data should be treated as ANSI escape codes.    */
-    private boolean wasAnsi = false;
+    private boolean styleChanged = false;
     /** Indicates whether incoming data should be sent to the protocol abstraction.*/
     private boolean wasSpecial = false;
     /** The style currently used for incoming data.                                */
     private FStyle current;
-    /** A buffer used for ANSI escape codes.                                       */
-    private final Vector<Byte> ansiBuffer = new Vector<>();
     /** A buffer used for broken unicode characters.                               */
     private final Vector<Byte> unicodeBuffer = new Vector<>();
 
@@ -96,7 +94,8 @@ class ConnectionDelegate implements ConnectionListener, ConnectionSender {
         defaultStyle = this.pane.getLogicalStyle();
         current = new FStyle();
         protocols = new Protocol(this, new SPPPlugin(),
-                                       new TelnetPlugin());
+                                       new TelnetPlugin(),
+                                       new ANSIPlugin(this));
 
         receiver.showMessageFrom(this, "Connecting...", null, 0);
 
@@ -167,164 +166,13 @@ class ConnectionDelegate implements ConnectionListener, ConnectionSender {
         System.err.println("--------------");
     }
 
-    /**
-     * Calculates the colour cube value using the given colour code
-     * and the given code.
-     *
-     * @param color the colour code
-     * @param code the code used for the calculations
-     * @return the colour cube value
-     */
-    private int color256CubeCalc(int color, int code) {
-        final var tmp = ((color - 16) / code) % 6;
-        return tmp == 0 ? 0 :
-                (14135 + 10280 * tmp) / 256;
+    public FStyle getCurrentStyle() {
+        return current;
     }
 
-    /**
-     * Returns the {@link Color} decoded from the given colour code.
-     * If it cannot be decoded, {@code null} is returned.
-     *
-     * @param colourCode the code of the 256-bit colour cube
-     * @return the decoded colour
-     */
-    private Color colourFrom256Bit(int colourCode) {
-        Color result = null;
-
-        if (colourCode < 16) {
-            switch (colourCode) {
-                case 0  -> result = new Color(0,   0,   0);
-                case 1  -> result = new Color(192, 0,   0);
-                case 2  -> result = new Color(0,   192, 0);
-                case 3  -> result = new Color(192, 192, 0);
-                case 4  -> result = new Color(0,   0,   192);
-                case 5  -> result = new Color(192, 0,   192);
-                case 6  -> result = new Color(0,   192, 192);
-                case 7  -> result = new Color(192, 192, 192);
-                case 8  -> result = new Color(128, 128, 128);
-                case 9  -> result = new Color(255, 0,   0);
-                case 10 -> result = new Color(0,   255, 0);
-                case 11 -> result = new Color(255, 255, 0);
-                case 12 -> result = new Color(0,   0,   255);
-                case 13 -> result = new Color(255, 0,   255);
-                case 14 -> result = new Color(0,   255, 255);
-                case 15 -> result = new Color(255, 255, 255);
-            }
-        } else if (colourCode < 232) {
-            result = new Color(color256CubeCalc(colourCode, 36),
-                               color256CubeCalc(colourCode, 6),
-                               color256CubeCalc(colourCode, 1));
-        } else if (colourCode < 256) {
-            final var value = ((float) ((2056 + 2570 * (colourCode - 232))) / 256) / 255;
-            result = new Color(value, value, value);
-        }
-
-        return result;
-    }
-
-    /**
-     * Parses the contents of the given ANSI buffer. If the buffer could not be parsed
-     * properly, {@code false} is returned and the {@link #current current style} is
-     * left untouched.
-     *
-     * @param buffer the ANSI buffer to parse
-     * @return whether the buffer was parsed successfully
-     */
-    private boolean parseAnsiBuffer(byte[] buffer) {
-        var str = new String(buffer, StandardCharsets.US_ASCII);
-
-        final var before = new FStyle(current, false);
-
-        try {
-            str = str.substring(1);
-            final var splits = str.split(";");
-            for (int i = 0; i < splits.length; ++i) {
-                switch (Integer.parseInt(splits[i])) {
-                    case 0  -> current = new FStyle();
-                    case 1  -> current.setBold(true);
-                    case 3  -> current.setItalic(true);
-                    case 4  -> current.setUnderlined(true);
-                    case 21 -> current.setBold(false);
-                    case 23 -> current.setItalic(false);
-                    case 24 -> current.setUnderlined(false);
-
-                    // Foreground
-                    case 30 -> current.setForeground(Color.black);
-                    case 31 -> current.setForeground(new Color(192, 0,   0));
-                    case 32 -> current.setForeground(new Color(0,   192, 0));
-                    case 33 -> current.setForeground(new Color(192, 192, 0));
-                    case 34 -> current.setForeground(new Color(0,   0,   192));
-                    case 35 -> current.setForeground(new Color(192, 0,   192));
-                    case 36 -> current.setForeground(new Color(0,   192, 192));
-                    case 37 -> current.setForeground(Color.lightGray);
-                    case 39 -> current.setForeground(null);
-                    case 90 -> current.setForeground(Color.darkGray);
-                    case 91 -> current.setForeground(new Color(255, 0,   0));
-                    case 92 -> current.setForeground(new Color(0,   255, 0));
-                    case 93 -> current.setForeground(new Color(255, 255, 0));
-                    case 94 -> current.setForeground(new Color(0,   0,   255));
-                    case 95 -> current.setForeground(new Color(255, 0,   255));
-                    case 96 -> current.setForeground(new Color(0,   255, 255));
-                    case 97 -> current.setForeground(Color.white);
-
-                    // Background
-                    case 40  -> current.setBackground(Color.black);
-                    case 41  -> current.setBackground(new Color(192, 0,   0));
-                    case 42  -> current.setBackground(new Color(0,   192, 0));
-                    case 43  -> current.setBackground(new Color(192, 192, 0));
-                    case 44  -> current.setBackground(new Color(0,   0,   192));
-                    case 45  -> current.setBackground(new Color(192, 0,   192));
-                    case 46  -> current.setBackground(new Color(0,   192, 192));
-                    case 47  -> current.setBackground(Color.lightGray);
-                    case 49  -> current.setBackground(null);
-                    case 100 -> current.setBackground(Color.darkGray);
-                    case 101 -> current.setBackground(new Color(255, 0,   0));
-                    case 102 -> current.setBackground(new Color(0,   255, 0));
-                    case 103 -> current.setBackground(new Color(255, 255, 0));
-                    case 104 -> current.setBackground(new Color(0,   0,   255));
-                    case 105 -> current.setBackground(new Color(255, 0,   255));
-                    case 106 -> current.setBackground(new Color(0,   255, 255));
-                    case 107 -> current.setBackground(Color.white);
-
-                    case 38 -> {
-                        ++i;
-
-                        final var code = Integer.parseInt(splits[i]);
-                        if (code == 5) {
-                            current.setForeground(colourFrom256Bit(Integer.parseInt(splits[i + 1])));
-                            ++i;
-                        } else if (code == 2) {
-                            current.setForeground(new Color(Integer.parseInt(splits[i + 1]),
-                                                            Integer.parseInt(splits[i + 2]),
-                                                            Integer.parseInt(splits[i + 3])));
-                            i += 3;
-                        }
-                    }
-
-                    case 48 -> {
-                        ++i;
-
-                        final var code = Integer.parseInt(splits[i]);
-                        if (code == 5) {
-                            current.setBackground(colourFrom256Bit(Integer.parseInt(splits[i + 1])));
-                            ++i;
-                        } else if (code == 2) {
-                            current.setBackground(new Color(Integer.parseInt(splits[i + 1]),
-                                                            Integer.parseInt(splits[i + 2]),
-                                                            Integer.parseInt(splits[i + 3])));
-                            i += 3;
-                        }
-                    }
-
-                    default -> System.err.println("Code not supported: " + splits[i] + "!");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            current = before;
-            return false;
-        }
-        return true;
+    public void setCurrentStyle(final FStyle newStyle) {
+        current = newStyle;
+        styleChanged = true;
     }
 
     /**
@@ -365,48 +213,40 @@ class ConnectionDelegate implements ConnectionListener, ConnectionSender {
             EventQueue.invokeLater(() -> receiver.showMessageFrom(this, "Connected.", Color.green, 5000));
         }
 
-        var document = pane.getDocument();
+        var document  = pane.getDocument();
 
         var text      = new Vector<>(unicodeBuffer);
         var ansiBegin = 0;
         var charCount = unicodeBuffer.size() > 0 ? 1 : 0;
+
+        var oldStyle  = new FStyle(current, false);
 
         unicodeBuffer.clear();
 
         var closedStyles = new Vector<Pair<Integer, FStyle>>();
 
         for (int i = 0; i < length; ++i) {
-            if (data[i] == 0x1B) {
-                wasAnsi   = true;
-                ansiBegin = charCount;
-                ansiBuffer.clear();
-            } else if (data[i] == 0x6D && wasAnsi) {
-                wasAnsi = false;
-
-                var oldCurrent = new FStyle(current, false);
-                if (parseAnsiBuffer(ByteHelper.castToByte(ansiBuffer.toArray(new Byte[0])))) {
-                    if (ansiBegin != 0 && closedStyles.isEmpty()) {
-                        closedStyles.add(new Pair<>(0, new FStyle(oldCurrent, false)));
-                    }
-                    closedStyles.add(new Pair<>(ansiBegin, new FStyle(current, false)));
-                } else {
-                    System.err.println("Error while parsing ANSI escape code!");
-                }
+            if (wasSpecial) {
+                wasSpecial = protocols.process(data[i]);
             } else {
-                if (wasAnsi) {
-                    ansiBuffer.add(data[i]);
-                } else if (wasSpecial) {
-                    wasSpecial = protocols.process(data[i]);
-                } else {
-                    wasSpecial = protocols.process(data[i]);
+                wasSpecial = protocols.process(data[i]);
 
-                    if (!wasSpecial) {
-                        text.add(data[i]);
-                        if (((data[i] & 0xff) >> 7) == 0 || ((data[i] & 0xff) >> 6) == 3) {
-                            ++charCount;
-                        }
+                if (!wasSpecial) {
+                    text.add(data[i]);
+                    if (((data[i] & 0xff) >> 7) == 0 || ((data[i] & 0xff) >> 6) == 3) {
+                        ++charCount;
                     }
+                } else {
+                    ansiBegin = charCount;
                 }
+            }
+            if (styleChanged) {
+                if (ansiBegin != 0 && closedStyles.isEmpty()) {
+                    closedStyles.add(new Pair<>(0, new FStyle(oldStyle, false)));
+                }
+                closedStyles.add(new Pair<>(ansiBegin, new FStyle(current, false)));
+                styleChanged = false;
+                oldStyle = current;
             }
         }
 
