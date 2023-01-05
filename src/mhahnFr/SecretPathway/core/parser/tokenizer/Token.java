67bac3c4255625710a44ac3d884e9b02b33d6bd0
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-05 19:01:03 +0100

    Added token record

---
+++ src/mhahnFr/SecretPathway/core/parser/tokenizer/Token.java
@@ -0,0 +1,31 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.core.parser.tokenizer;
+
+/**
+ * This record holds all information related to a specific Token.
+ *
+ * @param begin the beginning of this token in the text
+ * @param type the type of this token
+ * @param payload the payload held by this token
+ * @param end the end of this token in the text
+ */
+public record Token(int begin, TokenType type, Object payload, int end) {
+}
