c7f3904232c84f976708369b0b0c6db89d330a2f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-07 18:47:53 +0100

    Refactored the Tokenizer

--- src/mhahnFr/SecretPathway/core/parser/tokenizer/Tokenizer.java
+++ src/mhahnFr/SecretPathway/core/parser/tokenizer/Tokenizer.java
@@ -97,122 +97,121 @@ public class Tokenizer {
             } else {
                 return nextToken();
             }
-        } else if (stream.peek('(')) {
-            return new Token(stream.getIndex(), TokenType.LEFT_BRACKET, null, stream.skip(1));
-        } else if (stream.peek(')')) {
-            return new Token(stream.getIndex(), TokenType.RIGHT_BRACKET, null, stream.skip(1));
-        } else if (stream.peek('[')) {
-            return new Token(stream.getIndex(), TokenType.LEFT_BRACKET, null, stream.skip(1));
-        } else if (stream.peek(']')) {
-            return new Token(stream.getIndex(), TokenType.RIGHT_BRACKET, null, stream.skip(1));
-        } else if (stream.peek('{')) {
-            return new Token(stream.getIndex(), TokenType.LEFT_CURLY, null, stream.skip(1));
-        } else if (stream.peek('}')) {
-            return new Token(stream.getIndex(), TokenType.RIGHT_CURLY, null, stream.skip(1));
-        } else if (stream.peek("...")) {
-            return new Token(stream.getIndex(), TokenType.ELLIPSIS, null, stream.skip(3));
-        } else if (stream.peek('.')) {
-            return new Token(stream.getIndex(), TokenType.DOT, null, stream.skip(1));
-        } else if (stream.peek(',')) {
-            return new Token(stream.getIndex(), TokenType.COMMA, null, stream.skip(1));
-        } else if (stream.peek("::")) {
-            return new Token(stream.getIndex(), TokenType.SCOPE, null, stream.skip(2));
-        } else if (stream.peek(':')) {
-            return new Token(stream.getIndex(), TokenType.COLON, null, stream.skip(1));
-        } else if (stream.peek(';')) {
-            return new Token(stream.getIndex(), TokenType.SEMICOLON, null, stream.skip(1));
-        } else if (stream.peek("==")) {
-            return new Token(stream.getIndex(), TokenType.EQUALS, null, stream.skip(2));
-        } else if (stream.peek("!=")) {
-            return new Token(stream.getIndex(), TokenType.NOT_EQUAL, null, stream.skip(2));
-        } else if (stream.peek("<<")) {
-            return new Token(stream.getIndex(), TokenType.LEFT_SHIFT, null, stream.skip(2));
-        } else if (stream.peek(">>")) {
-            return new Token(stream.getIndex(), TokenType.RIGHT_SHIFT, null, stream.skip(2));
-        } else if (stream.peek("<=")) {
-            return new Token(stream.getIndex(), TokenType.LESS_OR_EQUAL, null, stream.skip(2));
-        } else if (stream.peek('<')) {
-            return new Token(stream.getIndex(), TokenType.LESS, null, stream.skip(1));
-        } else if (stream.peek(">=")) {
-            return new Token(stream.getIndex(), TokenType.GREATER_OR_EQUAL, null, stream.skip(2));
-        } else if (stream.peek('>')) {
-            return new Token(stream.getIndex(), TokenType.GREATER, null, stream.skip(1));
-        } else if (stream.peek("&&")) {
-            return new Token(stream.getIndex(), TokenType.AND, null, stream.skip(2));
-        } else if (stream.peek("||")) {
-            return new Token(stream.getIndex(), TokenType.OR, null, stream.skip(2));
-        } else if (stream.peek('!')) {
-            return new Token(stream.getIndex(), TokenType.NOT, null, stream.skip(1));
-        } else if (stream.peek('=')) {
-            return new Token(stream.getIndex(), TokenType.ASSIGNMENT, null, stream.skip(1));
-        } else if (stream.peek("->")) {
-            return new Token(stream.getIndex(), TokenType.ARROW, null, stream.skip(2));
-        } else if (stream.peek("|->")) {
-            return new Token(stream.getIndex(), TokenType.P_ARROW, null, stream.skip(3));
-        } else if (stream.peek('&')) {
-            return new Token(stream.getIndex(), TokenType.AMPERSAND, null, stream.skip(1));
-        } else if (stream.peek('|')) {
-            return new Token(stream.getIndex(), TokenType.PIPE, null, stream.skip(1));
-        } else if (stream.peek("??")) {
-            return new Token(stream.getIndex(), TokenType.DOUBLE_QUESTION, null, stream.skip(2));
-        } else if (stream.peek('?')) {
-            return new Token(stream.getIndex(), TokenType.QUESTION, null, stream.skip(1));
-        } else if (stream.peek("+=")) {
-            return new Token(stream.getIndex(), TokenType.ASSIGNMENT_PLUS, null, stream.skip(2));
-        } else if (stream.peek("-=")) {
-            return new Token(stream.getIndex(), TokenType.ASSIGNMENT_MINUS, null, stream.skip(2));
-        } else if (stream.peek("*=")) {
-            return new Token(stream.getIndex(), TokenType.ASSIGNMENT_STAR, null, stream.skip(2));
-        } else if (stream.peek("/=")) {
-            return new Token(stream.getIndex(), TokenType.ASSIGNMENT_SLASH, null, stream.skip(2));
-        } else if (stream.peek("%=")) {
-            return new Token(stream.getIndex(), TokenType.ASSIGNMENT_PERCENT, null, stream.skip(2));
-        } else if (stream.peek("++")) {
-            return new Token(stream.getIndex(), TokenType.INCREMENT, null, stream.skip(2));
-        } else if (stream.peek("--")) {
-            return new Token(stream.getIndex(), TokenType.DECREMENT, null, stream.skip(2));
-        } else if (stream.peek('+')) {
-            return new Token(stream.getIndex(), TokenType.PLUS, null, stream.skip(1));
-        } else if (stream.peek('-')) {
-            return new Token(stream.getIndex(), TokenType.MINUS, null, stream.skip(1));
-        } else if (stream.peek('"')) {
-            return new Token(stream.getIndex(), TokenType.STRING, readTill("\""), stream.getIndex());
-        } else if (stream.peek('\'')) {
-            return new Token(stream.getIndex(), TokenType.CHAR, readTill("'"), stream.getIndex());
-        } else if (stream.peek("#'")) {
-            return new Token(stream.getIndex(), TokenType.SYMBOL, readTill("'"), stream.getIndex());
-        } else if (stream.peek("#:")) {
-            return new Token(stream.getIndex(), TokenType.SYMBOL, readSymbol(), stream.getIndex());
         }
-        // TODO: Keywords
-        else if (stream.peek("new")) {
-            return new Token(stream.getIndex(), TokenType.NEW, null, stream.skip(3));
-        } else if (stream.peek("nil")) {
-            return new Token(stream.getIndex(), TokenType.NIL, null, stream.skip(4));
-        } else if (stream.peek("true")) {
-            return new Token(stream.getIndex(), TokenType.TRUE, null, stream.skip(4));
-        } else if (stream.peek("false")) {
-            return new Token(stream.getIndex(), TokenType.FALSE, null, stream.skip(5));
-        } else if (stream.peek("class")) {
-            return new Token(stream.getIndex(), TokenType.CLASS, null, stream.skip(5));
-        } else if (stream.peek("return")) {
-            return new Token(stream.getIndex(), TokenType.RETURN, null, stream.skip(6));
-        } else {
-            final var position = stream.getIndex();
-            final var buffer = new StringBuilder();
+        else if (stream.peek('('))   return new Token(stream.getIndex(), TokenType.LEFT_BRACKET,       null, stream.skip());
+        else if (stream.peek(')'))   return new Token(stream.getIndex(), TokenType.RIGHT_BRACKET,      null, stream.skip());
+        else if (stream.peek('['))   return new Token(stream.getIndex(), TokenType.LEFT_BRACKET,       null, stream.skip());
+        else if (stream.peek(']'))   return new Token(stream.getIndex(), TokenType.RIGHT_BRACKET,      null, stream.skip());
+        else if (stream.peek('{'))   return new Token(stream.getIndex(), TokenType.LEFT_CURLY,         null, stream.skip());
+        else if (stream.peek('}'))   return new Token(stream.getIndex(), TokenType.RIGHT_CURLY,        null, stream.skip());
+        else if (stream.peek("...")) return new Token(stream.getIndex(), TokenType.ELLIPSIS,           null, stream.skip(3));
+        else if (stream.peek('.'))   return new Token(stream.getIndex(), TokenType.DOT,                null, stream.skip());
+        else if (stream.peek(','))   return new Token(stream.getIndex(), TokenType.COMMA,              null, stream.skip());
+        else if (stream.peek("::"))  return new Token(stream.getIndex(), TokenType.SCOPE,              null, stream.skip(2));
+        else if (stream.peek(':'))   return new Token(stream.getIndex(), TokenType.COLON,              null, stream.skip());
+        else if (stream.peek(';'))   return new Token(stream.getIndex(), TokenType.SEMICOLON,          null, stream.skip());
+        else if (stream.peek("=="))  return new Token(stream.getIndex(), TokenType.EQUALS,             null, stream.skip(2));
+        else if (stream.peek("!="))  return new Token(stream.getIndex(), TokenType.NOT_EQUAL,          null, stream.skip(2));
+        else if (stream.peek("<<"))  return new Token(stream.getIndex(), TokenType.LEFT_SHIFT,         null, stream.skip(2));
+        else if (stream.peek(">>"))  return new Token(stream.getIndex(), TokenType.RIGHT_SHIFT,        null, stream.skip(2));
+        else if (stream.peek("<="))  return new Token(stream.getIndex(), TokenType.LESS_OR_EQUAL,      null, stream.skip(2));
+        else if (stream.peek('<'))   return new Token(stream.getIndex(), TokenType.LESS,               null, stream.skip());
+        else if (stream.peek(">="))  return new Token(stream.getIndex(), TokenType.GREATER_OR_EQUAL,   null, stream.skip(2));
+        else if (stream.peek('>'))   return new Token(stream.getIndex(), TokenType.GREATER,            null, stream.skip());
+        else if (stream.peek("&&"))  return new Token(stream.getIndex(), TokenType.AND,                null, stream.skip(2));
+        else if (stream.peek("||"))  return new Token(stream.getIndex(), TokenType.OR,                 null, stream.skip(2));
+        else if (stream.peek('!'))   return new Token(stream.getIndex(), TokenType.NOT,                null, stream.skip());
+        else if (stream.peek('='))   return new Token(stream.getIndex(), TokenType.ASSIGNMENT,         null, stream.skip());
+        else if (stream.peek("->"))  return new Token(stream.getIndex(), TokenType.ARROW,              null, stream.skip(2));
+        else if (stream.peek("|->")) return new Token(stream.getIndex(), TokenType.P_ARROW,            null, stream.skip(3));
+        else if (stream.peek('&'))   return new Token(stream.getIndex(), TokenType.AMPERSAND,          null, stream.skip());
+        else if (stream.peek('|'))   return new Token(stream.getIndex(), TokenType.PIPE,               null, stream.skip());
+        else if (stream.peek("??"))  return new Token(stream.getIndex(), TokenType.DOUBLE_QUESTION,    null, stream.skip(2));
+        else if (stream.peek('?'))   return new Token(stream.getIndex(), TokenType.QUESTION,           null, stream.skip());
+        else if (stream.peek("+="))  return new Token(stream.getIndex(), TokenType.ASSIGNMENT_PLUS,    null, stream.skip(2));
+        else if (stream.peek("-="))  return new Token(stream.getIndex(), TokenType.ASSIGNMENT_MINUS,   null, stream.skip(2));
+        else if (stream.peek("*="))  return new Token(stream.getIndex(), TokenType.ASSIGNMENT_STAR,    null, stream.skip(2));
+        else if (stream.peek("/="))  return new Token(stream.getIndex(), TokenType.ASSIGNMENT_SLASH,   null, stream.skip(2));
+        else if (stream.peek("%="))  return new Token(stream.getIndex(), TokenType.ASSIGNMENT_PERCENT, null, stream.skip(2));
+        else if (stream.peek("++"))  return new Token(stream.getIndex(), TokenType.INCREMENT,          null, stream.skip(2));
+        else if (stream.peek("--"))  return new Token(stream.getIndex(), TokenType.DECREMENT,          null, stream.skip(2));
+        else if (stream.peek('+'))   return new Token(stream.getIndex(), TokenType.PLUS,               null, stream.skip());
+        else if (stream.peek('-'))   return new Token(stream.getIndex(), TokenType.MINUS,              null, stream.skip());
+        else if (stream.peek('"'))   return new Token(stream.getIndex(), TokenType.STRING, readTill("\""), stream.getIndex());
+        else if (stream.peek('\''))  return new Token(stream.getIndex(), TokenType.CHAR,   readTill("'"),  stream.getIndex());
+        else if (stream.peek("#'"))  return new Token(stream.getIndex(), TokenType.SYMBOL, readTill("'"),  stream.getIndex());
+        else if (stream.peek("#:"))  return new Token(stream.getIndex(), TokenType.SYMBOL, readSymbol(),   stream.getIndex());
 
-            while (stream.hasNext() && !isSpecial(stream.peek())) {
-                buffer.append(stream.next());
-            }
+        return nextWord();
+    }
 
-            final var string = buffer.toString();
+    private Token nextWord() {
+        final var begin = stream.getIndex();
+        final var word  = readWord();
+        final var end   = stream.getIndex();
 
-            try {
-                return new Token(position, TokenType.INT, Integer.decode(string), stream.getIndex());
-            } catch (NumberFormatException ignored) {}
-
-            return new Token(position, TokenType.IDENTIFIER, string, stream.getIndex());
+        switch (word) {
+            case "#include"   -> { return new Token(begin, TokenType.INCLUDE,        null, end); }
+            case "inherit"    -> { return new Token(begin, TokenType.INHERIT,        null, end); }
+            case "private"    -> { return new Token(begin, TokenType.PRIVATE,        null, end); }
+            case "protected"  -> { return new Token(begin, TokenType.PROTECTED,      null, end); }
+            case "public"     -> { return new Token(begin, TokenType.PUBLIC,         null, end); }
+            case "override"   -> { return new Token(begin, TokenType.OVERRIDE,       null, end); }
+            case "deprecated" -> { return new Token(begin, TokenType.DEPRECATED,     null, end); }
+            case "new"        -> { return new Token(begin, TokenType.NEW,            null, end); }
+            case "this"       -> { return new Token(begin, TokenType.THIS,           null, end); }
+            case "nil"        -> { return new Token(begin, TokenType.NIL,            null, end); }
+            case "true"       -> { return new Token(begin, TokenType.TRUE,           null, end); }
+            case "false"      -> { return new Token(begin, TokenType.FALSE,          null, end); }
+            case "sizeof"     -> { return new Token(begin, TokenType.SIZEOF,         null, end); }
+            case "is"         -> { return new Token(begin, TokenType.IS,             null, end); }
+            case "class"      -> { return new Token(begin, TokenType.CLASS,          null, end); }
+            case "void"       -> { return new Token(begin, TokenType.VOID,           null, end); }
+            case "char"       -> { return new Token(begin, TokenType.CHAR_KEYWORD,   null, end); }
+            case "int"        -> { return new Token(begin, TokenType.INT_KEYWORD,    null, end); }
+            case "bool"       -> { return new Token(begin, TokenType.BOOL,           null, end); }
+            case "object"     -> { return new Token(begin, TokenType.OBJECT,         null, end); }
+            case "string"     -> { return new Token(begin, TokenType.STRING_KEYWORD, null, end); }
+            case "symbol"     -> { return new Token(begin, TokenType.SYMBOL_KEYWORD, null, end); }
+            case "mapping"    -> { return new Token(begin, TokenType.MAPPING,        null, end); }
+            case "any"        -> { return new Token(begin, TokenType.ANY,            null, end); }
+            case "mixed"      -> { return new Token(begin, TokenType.MIXED,          null, end); }
+            case "auto"       -> { return new Token(begin, TokenType.AUTO,           null, end); }
+            case "let"        -> { return new Token(begin, TokenType.LET,            null, end); }
+            case "if"         -> { return new Token(begin, TokenType.IF,             null, end); }
+            case "else"       -> { return new Token(begin, TokenType.ELSE,           null, end); }
+            case "while"      -> { return new Token(begin, TokenType.WHILE,          null, end); }
+            case "do"         -> { return new Token(begin, TokenType.DO,             null, end); }
+            case "foreach"    -> { return new Token(begin, TokenType.FOREACH,        null, end); }
+            case "for"        -> { return new Token(begin, TokenType.FOR,            null, end); }
+            case "switch"     -> { return new Token(begin, TokenType.SWITCH,         null, end); }
+            case "case"       -> { return new Token(begin, TokenType.CASE,           null, end); }
+            case "default"    -> { return new Token(begin, TokenType.DEFAULT,        null, end); }
+            case "break"      -> { return new Token(begin, TokenType.BREAK,          null, end); }
+            case "continue"   -> { return new Token(begin, TokenType.CONTINUE,       null, end); }
+            case "return"     -> { return new Token(begin, TokenType.RETURN,         null, end); }
+            case "try"        -> { return new Token(begin, TokenType.TRY,            null, end); }
+            case "catch"      -> { return new Token(begin, TokenType.CATCH,          null, end); }
+            case "operator"   -> { return new Token(begin, TokenType.OPERATOR,       null, end); }
         }
+
+        try {
+            return new Token(begin, TokenType.INT, Integer.decode(word), end);
+        } catch (NumberFormatException e) {
+            return new Token(begin, TokenType.IDENTIFIER, null, end);
+        }
+    }
+
+    private String readWord() {
+        final var buffer = new StringBuilder();
+        while (stream.hasNext() && !isSpecial(stream.peek())) {
+            buffer.append(stream.next());
+        }
+        if (buffer.isEmpty() && stream.hasNext()) {
+            // Unrecognized character used as word to prevent endless loop
+            buffer.append(stream.next());
+        }
+        return buffer.toString();
     }
 
     /**
@@ -220,7 +219,7 @@ public class Tokenizer {
      */
     private void skipWhitespaces() {
         while (stream.hasNext() && Character.isWhitespace(stream.peek())) {
-            stream.skip(1);
+            stream.skip();
         }
     }
 
