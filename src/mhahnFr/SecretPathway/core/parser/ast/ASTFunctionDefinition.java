a37f67275ba043192f24084db2329f0605523b49
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-28 22:05:06 +0100

    Added ASTFunctionDefinition

---
+++ src/mhahnFr/SecretPathway/core/parser/ast/ASTFunctionDefinition.java
@@ -0,0 +1,60 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.core.parser.ast;
+
+import mhahnFr.SecretPathway.core.parser.tokenizer.TokenType;
+import mhahnFr.utils.StreamPosition;
+
+/**
+ * This class represents an AST node for a function
+ * definition.
+ *
+ * @author mhahnFr
+ * @since 28.01.23
+ */
+public class ASTFunctionDefinition extends ASTExpression {
+    private final String name;
+    private final TokenType type;
+    private final ASTExpression[] body;
+
+    public ASTFunctionDefinition(final StreamPosition  begin,
+                                 final StreamPosition  end,
+                                 final TokenType       type,
+                                 final String          name,
+                                 final ASTExpression[] body) {
+        super(begin, end);
+
+        this.type = type;
+        this.name = name;
+        this.body = body;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TokenType getType() {
+        return type;
+    }
+
+    public ASTExpression[] getBody() {
+        return body;
+    }
+}
