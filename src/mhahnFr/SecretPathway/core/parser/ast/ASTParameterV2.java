d4ddf75d725a884e6cd3bfe414f80dcbeb1ae9ac
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-02-02 22:43:02 +0100

    Added new ASTParameter

---
+++ src/mhahnFr/SecretPathway/core/parser/ast/ASTParameterV2.java
@@ -0,0 +1,65 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.core.parser.ast;
+
+/**
+ * This class represents a declared function parameter
+ * as an AST node.
+ *
+ * @author mhahnFr
+ * @since 02.02.23
+ */
+public class ASTParameterV2 extends ASTExpression {
+    /** The declared type of this parameter. */
+    private final ASTExpression type;
+    /** The declared name of this parameter. */
+    private final ASTExpression name;
+
+    /**
+     * Constructs this AST node using the given information.
+     *
+     * @param type the declared type
+     * @param name the declared name
+     */
+    public ASTParameterV2(final ASTExpression type, final ASTExpression name) {
+        super(type.getBegin(), name.getEnd(), ASTType.PARAMETER);
+
+        this.type = type;
+        this.name = name;
+    }
+
+    /**
+     * Returns the declared type of this declared parameter.
+     *
+     * @return the declared type
+     */
+    public ASTExpression getType() {
+        return type;
+    }
+
+    /**
+     * Returns the declared name of this parameter.
+     *
+     * @return the declared parameter
+     */
+    public ASTExpression getName() {
+        return name;
+    }
+}
