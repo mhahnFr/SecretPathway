7ee48860890c8b130377c9f14ff92de9dd8a8459
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-02-23 19:37:56 +0100

    Documented ASTVisitor

--- src/mhahnFr/SecretPathway/core/lpc/parser/ast/ASTVisitor.java
+++ src/mhahnFr/SecretPathway/core/lpc/parser/ast/ASTVisitor.java
@@ -19,13 +19,38 @@
 
 package mhahnFr.SecretPathway.core.lpc.parser.ast;
 
+/**
+ * This interface defines an AST visitor.
+ *
+ * @author mhahnFr
+ * @since 31.01.23
+ */
 public interface ASTVisitor {
+    /**
+     * Visits the given {@link ASTExpression}.
+     *
+     * @param expression the expression to be visited
+     */
     void visit(final ASTExpression expression);
 
+    /**
+     * Returns whether the given type of AST node should be
+     * visited. Default implementation returns {@code true}.
+     *
+     * @param type the type in question
+     * @return whether to visit the given type in depth
+     */
     default boolean visitType(final ASTType type) {
         return true;
     }
 
+    /**
+     * Visits the given {@link ASTExpression} and returns whether
+     * to visit contained {@link ASTExpression}s.
+     *
+     * @param expression the node to be visited
+     * @return whether to visit contained nodes
+     */
     default boolean maybeVisit(final ASTExpression expression) {
         visit(expression);
         return visitType(expression.getASTType());
