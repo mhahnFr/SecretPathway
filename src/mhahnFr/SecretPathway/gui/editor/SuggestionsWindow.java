ef264bfb6d801b04ebce2ceace2336fd19379bd6
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-03-01 19:35:11 +0100

    Added documentation

--- src/mhahnFr/SecretPathway/gui/editor/SuggestionsWindow.java
+++ src/mhahnFr/SecretPathway/gui/editor/SuggestionsWindow.java
@@ -26,30 +26,65 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Vector;
 
+/**
+ * This class represents a window popup for displaying
+ * code suggestions.
+ *
+ * @author mhahnFr
+ * @since 01.03.23
+ */
 public class SuggestionsWindow extends JWindow {
+    /** The list with the suggestions to be displayed. */
     private final List<Definition> suggestions = new Vector<>();
 
+    /**
+     * Constructs this window.
+     */
     public SuggestionsWindow() {
         // TODO: UI
     }
 
+    /**
+     * Adds the given suggestion.
+     *
+     * @param suggestion the suggestion to be added
+     */
     public void addSuggestion(final Definition suggestion) {
         suggestions.add(suggestion);
     }
 
+    /**
+     * Adds the given suggestions.
+     *
+     * @param suggestions the suggestions to be added
+     */
     public void addSuggestions(final Collection<Definition> suggestions) {
         this.suggestions.addAll(suggestions);
     }
 
+    /**
+     * Updates the suggestions. Only suggestions in the given list
+     * will be displayed.
+     *
+     * @param newSuggestions the new suggestions to be displayed
+     */
     public void updateSuggestions(final Collection<Definition> newSuggestions) {
         suggestions.clear();
         suggestions.addAll(newSuggestions);
     }
 
+    /**
+     * Removes the given suggestion.
+     *
+     * @param suggestion the suggestion to be removed
+     */
     public void removeSuggestion(final Definition suggestion) {
         suggestions.remove(suggestion);
     }
 
+    /**
+     * Removes all suggestions.
+     */
     public void clearSuggestions() {
         suggestions.clear();
     }
