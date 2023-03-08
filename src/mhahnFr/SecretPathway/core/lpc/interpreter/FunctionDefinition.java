b6c0c009e553433ba0f6f310f8fd119e2d0b4df9
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-03-08 18:31:43 +0100

    Added function definition class

---
+++ src/mhahnFr/SecretPathway/core/lpc/interpreter/FunctionDefinition.java
@@ -0,0 +1,81 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.core.lpc.interpreter;
+
+import mhahnFr.SecretPathway.core.lpc.parser.ast.ASTType;
+import mhahnFr.SecretPathway.core.lpc.parser.ast.ASTTypeDefinition;
+
+import java.util.List;
+
+/**
+ * This class represents a function definition.
+ *
+ * @author mhahnFr
+ * @since 08.03.23
+ */
+public class FunctionDefinition extends Definition {
+    /** The list with the parameter definitions. */
+    private final List<Definition> parameters;
+
+    /**
+     * Constructs this function definition using the given information.
+     *
+     * @param begin      the beginning position
+     * @param name       the name of the function
+     * @param returnType the return type of the function
+     * @param parameters the list with the parameter definitions
+     */
+    public FunctionDefinition(final int               begin,
+                              final String            name,
+                              final ASTTypeDefinition returnType,
+                              final List<Definition>  parameters) {
+        super(begin, name, returnType, ASTType.FUNCTION_DEFINITION);
+
+        this.parameters = parameters;
+    }
+
+    /**
+     * Returns the parameter definitions of this function definition.
+     *
+     * @return the parameter definitions
+     */
+    public List<Definition> getParameters() {
+        return parameters;
+    }
+
+    @Override
+    public String toString() {
+        final var builder = new StringBuilder(super.getName());
+
+        builder.append('(');
+
+        final var iterator = parameters.listIterator();
+        while (iterator.hasNext()) {
+            final var parameter = iterator.next();
+            builder.append(parameter.getReturnType()).append(' ').append(parameter.getName());
+            if (iterator.hasNext()) {
+                builder.append(", ");
+            }
+        }
+        builder.append(')');
+
+        return builder.toString();
+    }
+}
