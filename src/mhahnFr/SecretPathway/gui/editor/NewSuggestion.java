1ca1be1487ecc4f5423436230d9fc2942293b532
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-03-13 20:16:35 +0100

    Added `new`-suggestion

---
+++ src/mhahnFr/SecretPathway/gui/editor/NewSuggestion.java
@@ -0,0 +1,47 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.gui.editor;
+
+/**
+ * This class represents a suggestion for the {@code new}
+ * statement.
+ *
+ * @author mhahnFr
+ * @since 13.03.23
+ */
+public class NewSuggestion implements Suggestion {
+    /** The suggestion {@link String}. */
+    private static final String string = "new(\"\")";
+
+    @Override
+    public String getSuggestion() {
+        return string;
+    }
+
+    @Override
+    public String getDescription() {
+        return "\"new\" statement";
+    }
+
+    @Override
+    public int getRelativeCursorPosition() {
+        return string.length() - 2;
+    }
+}
