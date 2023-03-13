6950d82967455548b7deb410d06a8c0bdfbea34b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-03-13 20:20:15 +0100

    Added `try`-suggestions

---
+++ src/mhahnFr/SecretPathway/gui/editor/TrySuggestion.java
@@ -0,0 +1,44 @@
+/*
+ * SecretPathway - A MUD client.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway. This program is free software:
+ * you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package mhahnFr.SecretPathway.gui.editor;
+
+/**
+ * This class represents a suggestion for the {@code try}
+ * statement.
+ *
+ * @author mhahnFr
+ * @since 13.03.23
+ */
+public class TrySuggestion implements Suggestion {
+    @Override
+    public String getSuggestion() {
+        return "try {\n    \n} catch {\n    \n}";
+    }
+
+    @Override
+    public String getDescription() {
+        return "try-catch statement";
+    }
+
+    @Override
+    public int getRelativeCursorPosition() {
+        return 10;
+    }
+}
