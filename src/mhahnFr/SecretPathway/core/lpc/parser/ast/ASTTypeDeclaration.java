afcc1680c09a8f28cf9208851590f1395cb715e9
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-03-20 19:59:46 +0100

    Implemented `nil` as valid for strings

--- src/mhahnFr/SecretPathway/core/lpc/parser/ast/ASTTypeDeclaration.java
+++ src/mhahnFr/SecretPathway/core/lpc/parser/ast/ASTTypeDeclaration.java
@@ -91,18 +91,22 @@ public class ASTTypeDeclaration extends ASTTypeDefinition {
     public boolean isAssignableFrom(ASTTypeDefinition other) {
         if (other instanceof final ASTTypeDeclaration declaration) {
             switch (type) {
-                case ANY          -> { return true; }
-                case OBJECT       -> { return declaration.type != TokenType.ANY  &&
+                case ANY            -> { return true; }
+                case OBJECT         -> { return declaration.type != TokenType.ANY  &&
                                               declaration.type != TokenType.BOOL &&
                                               declaration.type != TokenType.INT_KEYWORD;
-                                     }
+                                       }
                 case INT_KEYWORD,
-                     BOOL         -> { return (declaration.type == TokenType.INT_KEYWORD || declaration.type == TokenType.BOOL) &&
+                     BOOL           -> { return (declaration.type == TokenType.INT_KEYWORD || declaration.type == TokenType.BOOL) &&
                                                isArray == declaration.isArray;
-                                     }
-                default           -> { return type == declaration.type &&
+                                       }
+
+                case STRING_KEYWORD -> { return (declaration.type == TokenType.STRING_KEYWORD || declaration.type == TokenType.NIL) &&
+                                                 declaration.isArray == isArray;
+                                       }
+                default             -> { return type == declaration.type &&
                                               isArray == declaration.isArray;
-                                     }
+                                       }
             }
         }
         return false;
